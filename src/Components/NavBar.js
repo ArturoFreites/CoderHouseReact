29f6ad8811e763fc7aae522d4fcdca52096f3ab5
ArturoFreites <luisarturofreites1@gmail.com>
2022-03-07 20:43:18 -0300

    UltimaVersionDesafio2

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,5 @@
 
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
+import { Container, Nav, Navbar} from "react-bootstrap"
 
 function NavBar() {
 
@@ -7,7 +7,7 @@ function NavBar() {
 
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand href="index.html">
                         <img
                             src="https://www.greatplacetowork.com.ar/images/coderhouse-logo.png"
                             width="200"
@@ -18,13 +18,8 @@ function NavBar() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#features">Productos</Nav.Link>
+                        <Nav.Link href="#pricing">Nosotros</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
